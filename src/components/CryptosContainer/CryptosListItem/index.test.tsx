915fb9300f8e0a7e6323412d06b1e0939f1ce6b8
manudevcode <manyoh.3y@gmail.com>
2021-06-11 15:12:26 -0500

    feat: create CryptosListITem to show in CryptosList

---
+++ src/components/CryptosContainer/CryptosListItem/index.test.tsx
@@ -0,0 +1,69 @@
+
+import '@testing-library/jest-dom';
+import { render, RenderResult } from '@testing-library/react';
+import CryptosListItem from '.';
+
+/*
+ * CryptosListItem
+ * Add description for your component
+ */
+describe('<CryptosListItem />', () => {
+
+  let component: RenderResult;
+
+  beforeEach(() => {
+
+    let cryptoData = {
+      key: 'key',            
+      image: `https://www.cryptocompare.com/media/37746251/btc.png`,
+      price: 10,
+      marketCap: 11,
+      circulatingSupply: 12,
+      name: 'BTC',
+      // fields not showing
+      lowDay: 9,
+      highday: 13,
+      openDay: 9,
+    }
+
+    component = render(
+      <table>
+        <tbody>
+          <CryptosListItem key={cryptoData.key} crypto={cryptoData} />
+        </tbody>
+      </table>
+    );
+  })
+
+  it('should be in the DOM', () => {
+    expect(
+      component.container
+    ).toBeInTheDocument();
+  })
+
+  it('render correclty crpyto fields', () => {
+    let tdElements = component.container.getElementsByTagName('td');
+    expect(tdElements).toHaveLength(5)
+  }) 
+  
+  it('show crptyo name', () => {
+    let tdElements = component.container.querySelector('td:nth-child(1)');
+    expect(tdElements?.textContent).toEqual('BTC');
+  }) 
+
+  it('show crptyo price', () => {
+    let tdElements = component.container.querySelector('td:nth-child(2)');
+    expect(tdElements?.textContent).toEqual('$ 10');
+  }) 
+
+  it('show crptyo marketCap', () => {
+    let tdElements = component.container.querySelector('td:nth-child(3)');
+    expect(tdElements?.textContent).toEqual('11');
+  }) 
+
+  it('show crptyo circulatingSupply', () => {
+    let tdElements = component.container.querySelector('td:nth-child(4)');
+    expect(tdElements?.textContent).toEqual('12');
+  }) 
+})
+
---
+++ src/components/CryptosContainer/CryptosListItem/index.tsx
@@ -0,0 +1,58 @@
+import { FunctionComponent } from 'react'
+import { Crypto } from '../../../types/cryptoTypes';
+import { EyeIcon } from '@heroicons/react/solid'
+import FormatNumber from '../../FormatNumber';
+
+interface CryptosListItemProps {
+  key: string,
+  crypto: Crypto,
+}
+
+/**
+ * CryptosListItem
+ * @description Item to show in <CryptosList /> component
+ * @props CryptosListItemProps
+ */ 
+export const CryptosListItem: FunctionComponent<CryptosListItemProps> = ({ crypto }) => {
+    
+  return (
+    <tr data-testid="crypto" className="hover:bg-gray-200">
+      <td className="lg:px-6 px-2 py-1 whitespace-nowrap">
+        <span className="flex items-center">
+          <span className="flex-shrink-0 h-10 w-10">
+            <img className="h-10 w-10 rounded-full" src={crypto.image} alt={`Image of ${crypto.name}`} />
+          </span>
+          <span className="ml-4">
+            <span className="text-sm font-medium text-gray-900">{ crypto.name }</span>
+          </span>
+        </span>
+      </td>
+      <td className="lg:px-6 px-2 py-1 whitespace-nowrap">
+        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+        $ <span className="text-sm text-gray-900">
+          <FormatNumber number={ crypto.price } />
+        </span>
+        </span>
+      </td>
+      <td className="lg:px-6 px-2 py-1 whitespace-nowrap">
+        <span className="text-sm text-gray-500">
+          <FormatNumber number={ crypto.marketCap } />
+        </span>
+      </td>
+      <td className="lg:px-6 px-2 py-1 whitespace-nowrap text-sm text-gray-500">
+        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-100 text-blue-800">
+          <span className="text-sm text-gray-900">
+          <FormatNumber number={ crypto.circulatingSupply } />
+        </span>
+        </span>
+      </td>
+      <td className="lg:px-6 px-2 py-1 whitespace-nowrap text-right text-sm font-medium">
+        <a href="#" className="text-indigo-600 hover:text-indigo-900">
+          <EyeIcon className="block h-4 w-4 text-gray-800"/>
+        </a>
+      </td>
+    </tr>
+  )
+}
+
+export default CryptosListItem;
