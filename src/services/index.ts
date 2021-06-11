bf0d75a7b8411136d8415fbec41a7043bd397757
manudevcode <manyoh.3y@gmail.com>
2021-06-11 15:28:22 -0500

    feat: create api service

---
+++ src/services/index.ts
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+const BASE_URL = process.env.REACT_APP_API_URL;
+export const GetCryptos = () => axios.get(`${BASE_URL}/data/pricemultifull?fsyms=BTC,ETH,XRP,DOGE,BNB,LTC,ADA,BUSD,BCH,VET,DOT,EOS,SOL,SRM,USDT,BTT,TRX,FIL,LINK,MATIC,UNI,NEO,CHZ,ETC,THETA,XLM,BSV,LUNA,WIN,SXP&tsyms=USD`);
