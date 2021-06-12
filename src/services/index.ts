b92eeacc9a043ec0e530987eceea9ff64eda6d4d
manudevcode <manyoh.3y@gmail.com>
2021-06-11 23:37:37 -0500

    fix: change api endpoint to load 50 crpypto currencies

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,4 @@
 import axios from 'axios';
-
 const BASE_URL = process.env.REACT_APP_API_URL;
-export const GetCryptos = () => axios.get(`${BASE_URL}/data/pricemultifull?fsyms=BTC,ETH,XRP,DOGE,BNB,LTC,ADA,BUSD,BCH,VET,DOT,EOS,SOL,SRM,USDT,BTT,TRX,FIL,LINK,MATIC,UNI,NEO,CHZ,ETC,THETA,XLM,BSV,LUNA,WIN,SXP&tsyms=USD`);
+export const GetCryptos = () => axios.get(`${BASE_URL}/data/top/mktcapfull?limit=50&tsym=USD`);
+// export const GetCryptos = () => axios.get(`${BASE_URL}/data/pricemultifull?fsyms=BTC,ETH,XRP,DOGE,BNB,LTC,ADA,BUSD,BCH,VET,DOT,EOS,SOL,SRM,USDT,BTT,TRX,FIL,LINK,MATIC,UNI,NEO,CHZ,ETC,THETA,XLM,BSV,LUNA,WIN,SXP&tsyms=USD`);
